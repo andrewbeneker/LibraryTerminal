eee3f9fbaefe42ab3f59a925de1eb297de079ca5
Ubhumma <devonejohnson04@gmail.com>
2024-12-12 19:42:22 -0500

    initial commit

---
+++ LibraryTerminal Project/LibraryTerminal Project/LibraryCatalog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTerminal_Project
+{
+    internal class LibraryCatalog
+    {
+        public List<Book> Books { get; set; }
+
+        public LibraryCatalog()
+        {
+            Books = new List<Book>();
+        }
+
+        
+        
+        public void DisplayAvailableBooks()
+        {
+            foreach (Book book in Books)
+            {
+                if (book.Status == Status.OnShelf)
+                {
+                    Console.WriteLine($"{book.Title} by {book.Author}");
+                }
+            }
+        }
+        public void DisplayAllBooks()
+        {
+            foreach (Book book in Books)
+            {
+                Console.WriteLine($"{book.Title} by {book.Author} \n\tStatus: {book.Status} {(book.Status == Status.CheckedOut ? $"Estimated Date of Return {book.DueDate}" : "")}");
+                Console.WriteLine("");
+            }
+        }
+
+        //make a public void DonateBook method
+
+    } 
+}
---
+++ LibraryTerminal Project/LibraryTerminal Project/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTerminal_Project
+{
+    internal enum Status
+    {
+        CheckedOut = 1,
+        OnShelf,
+        Selected,
+        OutOfStock,
+    }
+}
