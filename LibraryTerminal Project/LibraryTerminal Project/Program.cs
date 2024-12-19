7cf970ce82cbbccb793bd4e12f715d1058bc00cf
Ubhumma <devonejohnson04@gmail.com>
2024-12-18 20:43:43 -0500

    Fixed whole library burning down situation

--- LibraryTerminal Project/LibraryTerminal Project/Program.cs
+++ LibraryTerminal Project/LibraryTerminal Project/Program.cs
@@ -2,38 +2,19 @@
 using System.Runtime.CompilerServices;
 Console.BackgroundColor = ConsoleColor.White;
 Console.ForegroundColor = ConsoleColor.Black;
+List<Book> prePickedBooks = new List<Book>()
+    {
+        new Book("Harry Potter", "J.K. Rowling", Status.OnShelf), new Book("Divergent", "Veronica Roth", Status.OnShelf),
+            new Book("Insurgent", "Veronica Roth", Status.OnShelf), //CHECK FOR INSERTING A DATETIME LITERAL
+            new Book("Allegiant", "Veronica Roth", Status.OnShelf),
+            new Book("Four", "Veronica Roth", Status.OnShelf), new Book("920 London", "Remy Boydell", Status.OnShelf),
+            new Book("To Kill A Mockingbird", "Harper Lee", Status.OnShelf), new Book("The Glass Castle", "Jeannette Walls", Status.OnShelf),
+            new Book("Deep Work", "Cal Newport", Status.OnShelf), new Book("Animal Farm", "George Orwell", Status.OnShelf),
+            new Book("The Great Gatsby", "F. Scott Fitzgerald", Status.OnShelf), new Book("The New Jim Crow: Mass Incarceration In The Age Of Colorblindness", "Michelle Alexander", Status.OnShelf),
+            new Book("Night", "Elie Wiesel", Status.OutOfStock), new Book("This Is Not a Personal Statement", "Tracy Badua", Status.CheckedOut),
+            new Book("The Odyssey", "Homer", Status.OnShelf),
+    };
 
-/*
-Write a console program which allows a user to search a library catalog and check out books.
-    *-Your solution must include some kind of a book class with a title, author, status, and due date if checked out.
-        -Status should be On Shelf or Checked Out (or other statuses you can imagine). 
-    *-12 items minimum; All stored in a list.
-    *Allow the user to:
-        *Display the entire list of books.  Format it nicely.
-        *Search for a book by author.
-        *Search for a book by title keyword.
-        *Select a book from the list to check out.
-            *If it’s already checked out, let them know.
-            *If not, check it out to them and set the due date to 2 weeks from today.
-    Return a book.  (You can decide how that looks/what questions it asks.)
-
-Optional enhancements:
-(Moderate) When the user quits, save the current library book list (including due dates and statuses) to the text file so the next time the program runs, it remembers.
-(Julius Caesar) Burn down the library of Alexandria and set human Civilization back by a few hundred years. 
-
- */
-
-//TODO: Make program less stream of text and options
-//TODO: SET UP A CART WITH EACH BOOK CHOSEN TO BE CHECKED OUT -
-//When checking out, first step is ask user if they're sure if they wanna add this book to their cart,
-// if y then add book to cart( stage the CheckOut), if no move on;
-//then ask if they wanna add anything else
-//if yes repeat, if no then move onto the CheckOut(IF THEY HAVE ANYTHING IN THEIR CART) process which will likely include confirming checkouts once more and after all confirmations are done then 
-//change status of chosen books in catalog to CheckedOut and Assign a DueDate to each of the books as Two Weeks from now
-//then empty the cart and create a sort of receipt.
-
-//Note: use selected Status so that user knows not to select the same book to check out. Only OnShelf items can be selected and Selected items can only be seleceted temporarily
-// they must either become CheckedOut or will turn back into OnShelf.
 
 //ADD STREAMWRITER AND READER TO SAVE INFO
 //Add greeting
@@ -53,6 +34,9 @@ string choice = "";
 
 
 
+Console.WriteLine("Welcome to the T.A.C Library!");
+Console.ReadKey();
+Console.Clear();
 
 try
 {
@@ -71,29 +55,28 @@ try
                 Book book = new Book(parts[0], parts[1], (Status)int.Parse(parts[2]), string.IsNullOrEmpty(parts[3]) ? null : parts[3], !string.IsNullOrEmpty(parts[4]) ? (DeweyDecimal)int.Parse(parts[4]) : null);
                 catalog.Books.Add(book);
             }
+            Console.WriteLine("Library catalog is accounted for ");
+        }
+        if ((line = reader.ReadLine()) == null)
+        {
+            Console.WriteLine("Library is suspiciously empty...\n" +
+        "Using backup catalog.");
+
+            catalog.Books.AddRange(prePickedBooks);
         }
     }
 }
 catch (FileNotFoundException)
 {
-    Console.WriteLine("File not found");
-    Console.WriteLine("Using prepicked list of Books");
-    List<Book> prePickedBooks = new List<Book>()
-    {
-        new Book("Harry Potter", "J.K. Rowling", Status.OnShelf), new Book("Divergent", "Veronica Roth", Status.OnShelf),
-            new Book("Insurgent", "Veronica Roth", Status.CheckedOut/*, DateTime.Parse("12/12/1212")*/), //CHECK FOR INSERTING A DATETIME LITERAL
-            new Book("Allegiant", "Veronica Roth", Status.OnShelf),
-            new Book("Four", "Veronica Roth", Status.OnShelf), new Book("920 London", "Remy Boydell", Status.OnShelf),
-            new Book("To Kill A Mockingbird", "Harper Lee", Status.OnShelf), new Book("The Glass Castle", "Jeannette Walls", Status.OnShelf),
-            new Book("Deep Work", "Cal Newport", Status.OnShelf), new Book("Animal Farm", "George Orwell", Status.OnShelf),
-            new Book("The Great Gatsby", "F. Scott Fitzgerald", Status.OnShelf), new Book("The New Jim Crow: Mass Incarceration In The Age Of Colorblindness", "Michelle Alexander", Status.OnShelf),
-            new Book("Night", "Elie Wiesel", Status.OutOfStock), new Book("This Is Not a Personal Statement", "Tracy Badua", Status.CheckedOut),
-            new Book("The Odyssey", "Homer", Status.OnShelf),
-    };
+    Console.WriteLine("Library is suspiciously empty...\n" +
+        "Using backup catalog.");
+
     catalog.Books.AddRange(prePickedBooks);
 }
 
-Console.WriteLine("Welcome to the T.A.C Library!");
+
+
+
 Console.ReadKey();
 Console.Clear();
 
@@ -181,6 +164,7 @@ void MainMenu()
                 Console.WriteLine("Press any key to forget libraries ever existed.");
                 Console.ReadKey();
                 Console.Clear();
+                File.Delete("catalog_File.txt");
             }
             else { }
             break;
@@ -271,7 +255,7 @@ while (true)
 Console.Clear();
 
 void CartMenu()
-    {
+{
     if (cart.Count > 0)
     {
         Console.WriteLine("Your cart has:");
@@ -330,7 +314,7 @@ using (StreamWriter catalogWriter = new StreamWriter("catalog_File.txt", false))
     {
         catalogWriter.WriteLine(book.ToString());
 
-    }  
+    }
 }
 
 
